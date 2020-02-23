b0d84b8cce536fcef69a18122b92617a21b504e6
Heungsub Lee <sub@subl.ee>
2020-02-23 05:41:48 +0000

    Change URL

--- pixelqart.py
+++ pixelqart.py
@@ -112,4 +112,4 @@ def main(href: str,
 
 
 if __name__ == '__main__':
-    main('https://subl.ee/', 'desired.png', 'necessary.png', concurrency=16)
+    main('https://subl.ee?▣', 'desired.png', 'necessary.png', concurrency=16)
