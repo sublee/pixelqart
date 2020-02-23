54150720c3deea4d9918733785e3f80ad24c2b39
Heungsub Lee <sub@subl.ee>
2020-02-23 12:46:48 +0000

    Evaluate decodability

--- pixelqart.py
+++ pixelqart.py
@@ -4,22 +4,27 @@ import itertools
 import os
 import random
 import tempfile
-from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor
 from threading import Event
 from typing import Tuple
 from urllib.parse import parse_qs, quote, urlparse
 
 import requests
-from PIL import Image
-from pyzbar.pyzbar import decode
+from PIL import Image, ImageFilter
+from pyzbar.pyzbar import decode as decode_barcode
 
 QRCODE_SIZE = (41, 41)  # QR Code v6
 QART_MARGIN = 4
 
 
 def split_design(filename: str) -> Tuple[Image.Image, Image.Image]:
-    NECESSARY_WHITE = (0, 255, 255, 255)  # cyan
-    NECESSARY_BLACK = (255, 0, 255, 255)  # magenta
+    """Splits a design image to the desired part and necessary part. The design
+    image may contain transparent pixels. Necessary In the design image,
+    necessary black and white should be replaced with blue (#00f) and yellow
+    (#ff0).
+    """
+    NECESSARY_BLACK = (0, 0, 255, 255)    # blue
+    NECESSARY_WHITE = (255, 255, 0, 255)  # yellow
 
     desired = Image.new('RGBA', QRCODE_SIZE)
     necessary = Image.new('RGBA', QRCODE_SIZE)
@@ -33,13 +38,13 @@ def split_design(filename: str) -> Tuple[Image.Image, Image.Image]:
 
         pixels = img.load()
 
-        for x, y in itertools.product(range(img.size[0]), range(img.size[1])):
-            if pixels[x, y] == NECESSARY_WHITE:
-                desired_pixels[x, y] = (255, 255, 255, 255)
-                necessary_pixels[x, y] = (255, 255, 255, 255)
-            elif pixels[x, y] == NECESSARY_BLACK:
+        for x, y in itertools.product(range(img.width), range(img.height)):
+            if pixels[x, y] == NECESSARY_BLACK:
                 desired_pixels[x, y] = (0, 0, 0, 255)
                 necessary_pixels[x, y] = (0, 0, 0, 255)
+            elif pixels[x, y] == NECESSARY_WHITE:
+                desired_pixels[x, y] = (255, 255, 255, 255)
+                necessary_pixels[x, y] = (255, 255, 255, 255)
             else:
                 desired_pixels[x, y] = pixels[x, y]
 
@@ -65,6 +70,10 @@ def search_qrcode(name: str,
     necessary part.
     """
     while True:
+        if found.is_set():
+            # Another thread has found.
+            break
+
         mask = random.randrange(8)
         orient = random.randrange(4)
         seed = random.getrandbits(32)
@@ -94,27 +103,66 @@ def search_qrcode(name: str,
 
         # The essential size of the QR Code is 49x49 (41x41 + margin 4px) but
         # it is scaled up 4 times.
-        assert qrcode.size == (196, 196)
+        size = (QRCODE_SIZE[0] + QART_MARGIN*2,
+                QRCODE_SIZE[1] + QART_MARGIN*2)
+
+        assert qrcode.width == 4 * size[0]
+        assert qrcode.height == 4 * size[1]
+        qrcode = qrcode.resize(size)
 
         # Paste the necessary part.
-        canvas = Image.new('RGBA', (49, 49), (0, 0, 0, 0))
-        canvas.paste(qrcode.resize((49, 49)))            # scale down 4 times
-        canvas.paste(necessary, (4, 4), mask=necessary)  # (4, 4): margin 4px
+        canvas = Image.new('RGBA', size, (0, 0, 0, 0))
+        canvas.paste(qrcode)
+        canvas.paste(necessary, (QART_MARGIN, QART_MARGIN), mask=necessary)
 
-        # Try to decode the QR Code with bigger size.
-        info = decode(canvas.resize((196, 196)))
+        info = decode_barcode(canvas.resize((canvas.width*2, canvas.height*2)))
+        ok = (len(info) == 1 and info[0].type == 'QRCODE')
+        if not ok:
+            continue
 
-        if info:
-            # Found!
-            filename = f'{name}.m{mask}o{orient}s{seed}.png'
-            canvas.save(filename)
-            print(f'Found: {filename}, {url}')
-            found.set()
-            break
+        # Found!
+        print(f'Found: {url}')
 
-        if found.is_set():
-            # Another thread has found.
-            break
+        # Evaluation is CPU-intensive.
+        with ProcessPoolExecutor(1) as ex:
+            fut = ex.submit(eval_qrcode, canvas)
+            score = fut.result()
+
+        filename = f'{name}-{score}-m{mask}o{orient}s{seed}.png'
+        canvas.save(filename)
+        print(f'Saved: {filename} (score: {score}, url: {url})')
+
+        found.set()
+        break
+
+
+def eval_qrcode(qrcode: Image.Image) -> int:
+    assert qrcode.width == QRCODE_SIZE[0] + QART_MARGIN*2
+    assert qrcode.height == QRCODE_SIZE[1] + QART_MARGIN*2
+
+    w, h = qrcode.size
+    resized = qrcode.resize((w*10, h*10))
+    expanded = Image.new('RGBA', (w*20, h*20), (0, 0, 0, 0))
+    expanded.paste(resized, (w*5, h*5), mask=resized)
+    qrcode = expanded
+
+    success = 1
+    for quality in range(1, 95+1):
+        img = qrcode.convert('RGB')
+
+        # Compress as JPEG
+        buf = io.BytesIO()
+        img.save(buf, format='jpeg', quality=quality)
+        buf.seek(0)
+        img = Image.open(buf)
+
+        # Decode
+        info = decode_barcode(img)
+        ok = (len(info) == 1 and info[0].type == 'QRCODE')
+        if ok:
+            success += 1
+
+    return success
 
 
 def main(href: str,
