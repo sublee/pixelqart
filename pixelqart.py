408ca5e5f0185e16b4c2f854e645b326ba7d7a24
Heungsub Lee <sub@subl.ee>
2020-02-23 06:06:43 +0000

    Split single design image

--- pixelqart.py
+++ pixelqart.py
@@ -1,15 +1,49 @@
 import base64
 import io
+import itertools
 import os
-import uuid
+import random
+import tempfile
 from concurrent.futures import ThreadPoolExecutor
-from random import getrandbits, randrange
 from threading import Event
+from typing import Tuple
 from urllib.parse import parse_qs, quote, urlparse
 
 import requests
 from PIL import Image
 from pyzbar.pyzbar import decode
+
+QRCODE_SIZE = (41, 41)  # QR Code v6
+QART_MARGIN = 4
+
+
+def split_design(filename: str) -> Tuple[Image.Image, Image.Image]:
+    NECESSARY_WHITE = (0, 255, 255, 255)  # cyan
+    NECESSARY_BLACK = (255, 0, 255, 255)  # magenta
+
+    desired = Image.new('RGBA', QRCODE_SIZE)
+    necessary = Image.new('RGBA', QRCODE_SIZE)
+
+    desired_pixels = desired.load()
+    necessary_pixels = necessary.load()
+
+    with Image.open(filename) as img:
+        assert img.mode == 'RGBA'
+        assert img.size == QRCODE_SIZE, img.size
+
+        pixels = img.load()
+
+        for x, y in itertools.product(range(img.size[0]), range(img.size[1])):
+            if pixels[x, y] == NECESSARY_WHITE:
+                desired_pixels[x, y] = (255, 255, 255, 255)
+                necessary_pixels[x, y] = (255, 255, 255, 255)
+            elif pixels[x, y] == NECESSARY_BLACK:
+                desired_pixels[x, y] = (0, 0, 0, 255)
+                necessary_pixels[x, y] = (0, 0, 0, 255)
+            else:
+                desired_pixels[x, y] = pixels[x, y]
+
+    return desired, necessary
 
 
 def upload_image(filename: str) -> str:
@@ -21,16 +55,20 @@ def upload_image(filename: str) -> str:
     return qs['i'][0]
 
 
-def search_qrcode(href: str,
+def search_qrcode(name: str,
+                  href: str,
                   uploaded_image_id: str,
                   necessary: Image.Image,
-                  output: str,
                   found: Event,
                   ) -> None:
     """Finds a QR Code including a pixel-art. A found QR Code must include the
     necessary part.
     """
     while True:
+        mask = random.randrange(8)
+        orient = random.randrange(4)
+        seed = random.getrandbits(32)
+
         # https://github.com/rsc/swtch/blob/master/qrweb/play.go#L145
         url = ('https://research.swtch.com/qr/draw?x=0&y=0&c=0&'
                f'i={uploaded_image_id}&'
@@ -40,9 +78,9 @@ def search_qrcode(href: str,
                't=0&'  # Dither (not implemented)
                'z=0&'  # Scale of source image
                f'u={quote(href, safe="")}&'
-               f'm={randrange(8)}&'    # Mask pattern (0-7)
-               f'o={randrange(4)}&'    # Rotation (0-3)
-               f's={getrandbits(32)}'  # Random seed (int64)
+               f'm={mask}&'    # Mask pattern (0-7)
+               f'o={orient}&'  # Rotation (0-3)
+               f's={seed}'     # Random seed (int64)
                )
         print(f'Trying: {url}')
 
@@ -68,7 +106,7 @@ def search_qrcode(href: str,
 
         if info:
             # Found!
-            filename = os.path.join(output, f'{uuid.uuid4()}.png')
+            filename = f'{name}.m{mask}o{orient}s{seed}.png'
             canvas.save(filename)
             print(f'Found: {filename}, {url}')
             found.set()
@@ -80,26 +118,23 @@ def search_qrcode(href: str,
 
 
 def main(href: str,
-         desired_filename: str,
-         necessary_filename: str,
+         design_filename: str,
          concurrency: int,
          ) -> None:
-    os.makedirs('found/', exist_ok=True)
+    name, png = os.path.splitext(os.path.basename(design_filename))
+    assert png.lower() == '.png'
 
-    desired = Image.open(desired_filename)
-    necessary = Image.open(necessary_filename)
-    assert desired.size == (41, 41), desired.size
-    assert necessary.size == (41, 41), necessary.size
+    desired, necessary = split_design(design_filename)
 
-    desired.close()
-    uploaded_image_id = upload_image(desired_filename)
+    with desired, tempfile.NamedTemporaryFile() as f:
+        desired.save(f, format='PNG')
+        uploaded_image_id = upload_image(f.name)
 
     found = Event()
     ex = ThreadPoolExecutor()
     for i in range(concurrency):
         ex.submit(search_qrcode,
-                  href, uploaded_image_id, necessary,
-                  output='found/', found=found)
+                  name, href, uploaded_image_id, necessary, found)
 
     try:
         found.wait()
@@ -112,4 +147,4 @@ def main(href: str,
 
 
 if __name__ == '__main__':
-    main('https://subl.ee?▣', 'desired.png', 'necessary.png', concurrency=16)
+    main('https://subl.ee?▣', 'griffith2020.png', concurrency=16)
